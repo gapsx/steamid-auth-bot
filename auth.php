58a4775148847ab23a32405253a7664077e97876
Gabriel <68157259+gapsx@users.noreply.github.com>
2020-12-11 21:29:46 -0300

    fixed "$db_host"

--- auth.php
+++ auth.php
@@ -4,7 +4,6 @@
     $db_name = '************';
     $db_pass = '************';
     $connect = mysqli_connect($db_host, $db_username, $db_pass) or die("erro ao conectar a database1");
-    mysqli_select_db($connect, $db_name) or die("erro ao conectar a database2");
     $sql= "SELECT * FROM `idsbot` WHERE steamid";
     $result = mysqli_query($connect, $sql);
         while($row = mysqli_fetch_array($result)) {
