4febd200be3ed41e68e23666afb683d092e429ce
Gabriel <68157259+gapsx@users.noreply.github.com>
2021-01-16 13:58:29 -0300

    Update auth.php

--- auth.php
+++ auth.php
@@ -3,7 +3,7 @@
     $db_username = '************';
     $db_name = '************';
     $db_pass = '************';
-    $connect = mysqli_connect($db_host, $db_username, $db_pass) or die("erro ao conectar a database1");
+    $connect = mysqli_connect($db_host, $db_username, $db_pass) or die("erro ao conectar a database");
     $sql= "SELECT * FROM `idsbot` WHERE steamid";
     $result = mysqli_query($connect, $sql);
         while($row = mysqli_fetch_array($result)) {
